10f9ab011d9a57ecef97a6a792d217978a11eeb3
RishabhRai <rrishabhrai19105@gmail.com>
2022-08-28 17:16:48 +0530

    addedd

---
+++ scripts/landing.js
@@ -0,0 +1,275 @@
+let landingPage = `<img
+  src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-modularblock-oneBytwo1A-Women-23Aug2022A.jpg"
+  alt=""
+/>
+<hr />
+<img
+  src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-12modblock-oneBythree-A-StripBanner1-23Aug2022.jpg"
+  alt=""
+/>
+<hr />
+<img
+  src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-10modblock-oneBythree-A-Women-23Aug2022.jpg"
+  alt=""
+/>
+<hr />
+<img
+  src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-9modblock-StripBanner2-D-23Aug2022.jpg"
+  alt=""
+/>
+<hr />
+<p>Trending Festive Wear</p>
+<div id="first">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-13modblock-oneBythree-A-Women-23Aug2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-13modblock-oneBythree-B-Women-23Aug2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-13modblock-oneBythree-C-Women-23Aug2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Trending Adds-On</p>
+<div id="second">
+  <div>
+    <img
+      src="https://www.designrush.com/uploads/users/customer-12/image_1526988214_ofcXaxy7pNFs5CvLfMzwtabKeHdBpsXvwvUWaVyY.jpeg"
+      alt=""
+    />
+    <h3>Upto 40% OFF</h3>
+  </div>
+  <div>
+    <img
+      src="https://cdn11.bigcommerce.com/s-pkla4xn3/images/stencil/1280x1280/products/17375/159272/A3313-Cusrom-logo-Watch-photo-print-Watches-watch-face-Printing-Wristwatch-Customized-Unique-DIY-Gift-For__76537.1545023850.jpg?c=2?imbypass=on"
+      alt=""
+    />
+    <h3>Upto 70% OFF</h3>
+  </div>
+  <div>
+    <img
+      src="https://assets.ajio.com/medias/sys_master/root/20220119/8NwM/61e832caaeb2695cdd1d76a4/-473Wx593H-410308566-252-MODEL.jpg"
+      alt=""
+    />
+    <h3>Upto 50% OFF</h3>
+  </div>
+  <div>
+    <img
+      src="https://btsmerchshop.com/wp-content/uploads/2018/12/11100-bc00eb.jpeg"
+      alt=""
+    />
+    <h3>Upto 60% OFF</h3>
+  </div>
+  <div>
+    <img
+      src="https://img.freepik.com/premium-psd/black-perfume-bottle-logo-mockup-black-tree-branch-background-3d-render_360590-500.jpg?w=2000"
+      alt=""
+    />
+    <h3>Upto 50% OFF</h3>
+  </div>
+  <div>
+    <img
+      src="https://img.freepik.com/free-photo/glamour-blond-woman-wearings-black-bikini-sunglasses-lies-sandy-beach_197531-22896.jpg"
+      alt=""
+    />
+    <h3>Upto 50% OFF</h3>
+  </div>
+</div>
+<hr />
+<p>Wardrobe Essentials</p>
+<div id="third">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-15modularblock-oneBythree-A-Women-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-15modularblock-oneBythree-B-Women-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-15modularblock-oneBythree-C-Women-24Aug2022A.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Top Picks</p>
+<div id="fourth">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-D-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-E-26July2022A.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-Women-modblock-oneBythree-F-26July2022A.jpg"
+    alt=""
+  />
+</div>
+<p>Essentials Range</p>
+<div id="fivth">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-14modularblock-oneBythree-A-Women-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-14modularblock-oneBythree-B-Women-26July2022A.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-14modularblock-oneBythree-C-Women-26July2022A.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Wardrobe -Must Haves</p>
+<div id="sixth">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-modularblock-oneBythree-A-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-modularblock-oneBythree-B-24June2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-modularblock-oneBythree-C-26July2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Trendsetters</p>
+<div id="seventh">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-8modblock-oneBythree-A-Women-04March2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-8modblock-oneBythree-B-Women-04March2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-8modblock-oneBythree-C-Women-04March2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Most Loved Styles</p>
+<div id="eight">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-6modblk-oneBythree-A-Women-03Aug2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-6modblk-oneBythree-B-Women-03Aug2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-6modblk-oneBythree-C-Women-03Aug2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Editor's Curated List</p>
+<div id="ninth">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-modblock7-oneBythree-A-Women-08April2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-modblock7-oneBythree-B-Women-08April2022.jpg"
+    alt=""
+  />
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-modblock7-oneBythree-C-Women-08April2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Flash Sale For Today</p>
+<div id=" ten">
+  <img
+    src="https://lmsin.net/cdn-cgi/image/w=1232,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-women-modularblock1-A-08July2022.jpg"
+    alt=""
+  />
+</div>
+<hr />
+<p>Trending Ethnic Bottoms</p>
+<div id="eleven">
+  <div>
+    <img
+      src="https://lmsin.net/cdn-cgi/image/h=600,w=444,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010636030-Pink-Pink-1000010636030_01-2100.jpg"
+      alt=""
+    />
+    <h3>₹779 <span>₹1299</span></h3>
+    <p>BIBA Women Printed Elasticated Palazzos</p>
+  </div>
+  <div>
+    <img
+      src="https://lmsin.net/cdn-cgi/image/h=600,w=444,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010210796-White-Natural-1000010210796_01-2100.jpg"
+      alt=""
+    />
+    <h3>₹999 <span>₹1599</span></h3>
+    <p>BIBA Women Printed Elasticated Straight Pants</p>
+  </div>
+  <div>
+    <img
+      src="https://lmsin.net/cdn-cgi/image/h=600,w=444,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000011024053-Red-Red-1000011024053_01-2100.jpg"
+      alt=""
+    />
+    <h3>₹1399 <span>₹1999</span></h3>
+    <p>BIBA Women Printed Regular Fit Elasticated Skirt</p>
+  </div>
+  <div>
+    <img
+      src="https://lmsin.net/cdn-cgi/image/h=600,w=444,q=60,fit=cover/https://aaeff43fe32172cbcecc-ae2a4e9a8cbc330ede5588dedf56886e.lmsin.net/lifestyle/1000010210871-Red-Maroon-1000010210871_01-2100.jpg"
+      alt=""
+    />
+    <h3>₹999 <span>₹1299</span></h3>
+    <p>BIBA Women Embellished Straight Pants</p>
+  </div>
+</div>
+<button>
+  SEE ALL
+</button>
+<hr>
+<p>
+  Best Sellers
+</p>
+  <div id="twelve">
+      <img src="https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-5modblock-oneBytwo1-C-Women-09yAug2022.jpg" alt="">
+      <img src="https://lmsin.net/cdn-cgi/image/w=616,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-5modblock-oneBytwo1-D-Women-09Aug2022.jpg" alt="">
+  </div>
+  <hr>
+  <p>Shop by Department</p>
+  <div id="thirteen">
+      <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-11modblock-oneBythree-A-Women-04March2022A.jpg" alt="">
+      <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-11modblock-oneBythree-B-Women-12April2022.jpg" alt="">
+      <img src="https://lmsin.net/cdn-cgi/image/w=500,q=70,fit=cover/https://70415bb9924dca896de0-34a37044c62e41b40b39fcedad8af927.lmsin.net/LS-Fest/LS-new/desktop-dept-11modblock-oneBythree-C-Women-04March2022A.jpg" alt="">
+  </div>
+  <hr>
+  <div id="fourteen">
+      <div>
+          <h2>Subscribe to our awesome Emails.</h2>
+          <p>Get our latest offers and news straight in your inbox.</p>
+          <input type="text" placeholder="Please enter an email address">
+          <button>Subscribe</button>
+      </div>
+      <div>
+          <h2>Download our Apps</h2>
+          <p>Shop our products and offers on-the-go.</p>
+          <div>
+              <img src="https://1000logos.net/wp-content/uploads/2020/08/apple-app-store-logo.jpg" alt="">
+              <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/7/78/Google_Play_Store_badge_EN.svg/2560px-Google_Play_Store_badge_EN.svg.png" alt="">
+          </div>
+      </div>
+  </div>
+  <hr> `;
+
+let landing = document.querySelector("#parent");
+landing.innerHTML = landingPage;
